33ce6bc7e42735592a6b975c1db6c765f71689de
Guilhermebarb <glimabarbosa069@gmail.com>
2025-04-14 22:17:35 -0300

    Troca better-sqlite3 por sqlite3 para compatibilidade com Render

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  dialect: 'better-sqlite3',
+  dialect: 'sqlite',
   storage: './backend/database/database.sqlite'
 });
 
